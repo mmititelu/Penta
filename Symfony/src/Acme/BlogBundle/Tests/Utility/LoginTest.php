6e872456daf3c115ee7248142782345a4634f32c
Mihaela Mititelu <mmititelu@pentalog.fr>
2013-08-02 11:05:34 +0300

    tests

--- Symfony/src/Acme/BlogBundle/Tests/Utility/LoginTest.php
+++ Symfony/src/Acme/BlogBundle/Tests/Utility/LoginTest.php
@@ -3,17 +3,21 @@ namespace Acme\BlogBundle\Tests\Utility;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class SubmitTest extends WebTestCase
+class LoginTest extends WebTestCase
 {
-     public function testShow()
+     public function testLogin()
     {
         $client = static::createClient();
         $client->followRedirects();
         $crawler = $client->request('GET', 'login');
-        //print_r($crawler->html());
-               
         $form = $crawler->selectButton('submit')->form();
-        $crawler = $client->submit($form, array('username' => 'admin', 'password' => 'admin'));
+
+        // set some values
+        $form['_username'] = 'admin';
+        $form['_password'] = 'admin';
+        
+        // submit the form
+        $crawler = $client->submit($form);
         $this->assertGreaterThan(
             0,
             $crawler->filter('html:contains("Blog homepage")')->count()
